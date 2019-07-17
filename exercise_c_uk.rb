7444417d30852df1d4a7708926f8ab8b1bed671c
KevSeenan <kev.seenan@gmail.com>
2019-07-18 00:11:05 +0100

    Commit for adding Nothern Ireland hash

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -13,14 +13,27 @@ united_kingdom = [
     name: "England",
     population: 53010000,
     capital: "London"
+  },
+  {
+    name: "Nothern Ireland",
+    population: 1811000,
+    capital: "Belfast"
   }
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
-united_kingdom[1][:capital].replace("Cardiff")
-p united_kingdom
+# united_kingdom[1][:capital].replace("Cardiff")
+# p united_kingdom
 
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+# {
+#   name: "Nothern Ireland",
+#   population: 1,811000
+#   capital:Belfast
+# }
 # 3. Use a loop to print the names of all the countries in the UK.
 # 4. Use a loop to find the total population of the UK.
+
+p united_kingdom
