8d709d08c3d5cde08f064d3c07efa77aa7c8d2f9
KevSeenan <kev.seenan@gmail.com>
2019-07-17 16:02:25 +0100

    Commit for adding Polmont

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -4,9 +4,11 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 # stops.push("Edinburgh Waverley")
 # p stops
 # 2. Add `"Glasgow Queen St"` to the start of the array
-stops.unshift("Glasgow Queen St")
-p stops
+# stops.unshift("Glasgow Queen St")
+# p stops
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(3, "Polmont")
+p stops
 # 4. Work out the index position of `"Linlithgow"`
 # 5. Remove `"Livingston"` from the array using its name
 # 6. Delete `"Cumbernauld"` from the array by index
